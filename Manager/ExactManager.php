a1c73bf5e8e11b8feeed65c39285474a4f51a35f
JeffersonOnDev <jefferson@zangra.com>
2019-09-21 15:38:47 +0200

    sf4.2.5 sf3.4 [FIX] parameter wasn't supported for ressource BillOfMaterialMa…

--- Manager/ExactManager.php
+++ Manager/ExactManager.php
@@ -5,109 +5,117 @@ namespace aibianchi\ExactOnlineBundle\Manager;
 use Doctrine\ORM\EntityManager;
 use aibianchi\ExactOnlineBundle\DAO\Connection;
 use aibianchi\ExactOnlineBundle\DAO\Exception\ApiException;
+use aibianchi\ExactOnlineBundle\Model\BillOfMaterialMaterial;
 
 /**
  * Exact Manager
  * Author: Jefferson Bianchi
  * Mail  : Jefferson@aibianchi.com
  */
-class ExactManager
-{
+class ExactManager {
+
     private $list = array();
     private $model;
     private $config;
     private $em;
+    private $logger;
 
 
-    public function __construct(EntityManager $em)
-    {
-        $this->em = $em;
+    public function __construct(EntityManager $em){
+            $this->em = $em;
     }
 
-    public function setConfig(array $config)
-    {
+    public function setConfig($config){
         $this->config = $config;
+
+
     }
+
 
     /**
     * @return void
     */
-    public function init(string $code = null, string $country)
-    {
-        if (!$country) {  throw new Exception("Country can't be null, please check packages/exact_online.yaml"); }
-        try {
-            Connection::setConfig($country, $this->config, $this->em);
+	public function init($code){
 
-            if (Connection::isExpired()) {
-                if ($code == null) {
+        try{
+    		Connection::setConfig($this->config, $this->em, $this->logger);
+
+            if (Connection::isExpired()){
+
+                if ($code == null){
                     Connection::getAuthorization();
                 }
                 Connection::setCode($code);
                 Connection::getAccessToken();
             }
-        } catch (ApiException $e) {
-            throw new Exception("Can't initiate connection: ", $e->getCode());
-        }
-    }
 
-    public function refreshToken(string $country)
-    {
-        Connection::setConfig($country, $this->config, $this->em);
-        Connection::refreshAccessToken($country);
+        }catch (ApiException $e) {
+                throw new Exception("Can't initiate connection: ", $e->getCode());
+
+        }
+
+	}
+
+    public function refreshToken(){
+
+        Connection::setConfig($this->config, $this->em, $this->logger);
+        Connection::refreshAccessToken();
     }
 
     /**
     * @return Object
     */
-    public function getModel($name)
-    {
-        try {
+	public function getModel($name){
+
+        try{
             $classname   = $cname = "aibianchi\\ExactOnlineBundle\\Model\\".$name;
             $this->model = new $classname();
             return $this;
-        } catch (ApiException $e) {
+        }catch (ApiException $e) {
             throw new ApiException("Model doesn't existe : ", $e->getStatusCode());
         }
-    }
+	}
 
     /**
     * @return void
     */
-    public function persist($entity)
-    {
+    public function persist($entity){
+
         $json = $entity->toJson();
         Connection::Request($entity->getUrl(), "POST", $json);
+
     }
 
     /**
     * @return void
     */
-    public function remove($entity)
-    {
+    public function remove($entity){
+
         $json     = $entity->toJson();
         $keyField = $this->getKeyField();
         $getter   = "get".$keyField;
         $url      = $entity->getUrl()."(guid'".$entity->$getter()."')";
         Connection::Request($url, "DELETE", $json);
+
     }
 
     /**
     * @return void
     */
-    public function update($entity)
-    {
+    public function update($entity){
+
         $json     = $entity->toJson();
         $keyField = $this->getKeyField();
         $getter   = "get".$keyField;
         $url      = $entity->getUrl()."(guid'".$entity->$getter()."')";
         Connection::Request($url, "PUT", $json);
+
     }
 
     /**
     * @return integer
     */
-    public function count()
-    {
+    public function count(){
         $url  =  $this->model->getUrl()."\\"."\$count";
         $data =  Connection::Request($url, "GET");
         return $data;
@@ -119,37 +127,30 @@ class ExactManager
     * https://support.exactonline.com/community/s/knowledge-base#All-All-DNO-Content-resttips
     * @return Object Collection
     */
-    public function getList(int $page = 1, int $maxPerPage = 5)
-    {
-        if ($maxPerPage>=60) {
+    public function getList($page = 1, $maxPerPage = 5){
+
+        if ($maxPerPage>=60){
             throw new ApiException("60 records maximum per page", 406);
         }
 
         $total    = $this->count();
 
-        if ($maxPerPage>$total) {
+        if ($maxPerPage>$total){
             throw new ApiException("Maximum records is: ".$total, 406);
         }
 
         $nbrPages = ceil($total/$maxPerPage);
         $skip     = ($page*$maxPerPage)-$maxPerPage;
-        $url      = $this->model->getUrl()."\\?"."\$skip=".$skip."&\$top=".$maxPerPage;
+
+        if ($this->model instanceof BillOfMaterialMaterial){
+            $url      = $this->model->getUrl()."\\?"."&\$top=".$maxPerPage;
+        }else{
+            $url      = $this->model->getUrl()."\\?"."\$skip=".$skip."&\$top=".$maxPerPage;
+        }
+
         $data     = Connection::Request($url, "GET");
 
-        return $this->isArrayCollection($this->model, $data);
-    }
-
-    /**
-     * You could use raw url with ODATA format
-     * ex : $rawUrl = "$filter=(baseRate ge 60 and baseRate lt 300) and city eq 'Los Angeles'"
-     * @param  string $rawUrl
-     * @return array
-     */
-    public function findByRawUrl($rawUrl){
-
-        $url = $this->model->getUrl()."\?"."\$".$rawUrl;
-        $data =  Connection::Request($url, "GET");
-        return $this->isArrayCollection($this->model, $data);
+        return $this->isArrayCollection($this->model,$data);
 
     }
 
@@ -159,60 +160,56 @@ class ExactManager
     *    5,                              // limit
     *    @return array
     */
-    public function findBy(array $criteria, array $select = null, array $orderby = null, int $limit  = 5)
-    {
+    public function findBy(array $criteria, array $select = null, array $orderby = null, $limit  = 5){
+
         $url = $this->model->getUrl()."\?"."\$filter=".key($criteria)." eq '".current($criteria)."'";
 
-        if ($select != null) {
+        if ($select != null){
             $url = $url."&\$select=";
-            for ($i=0; $i<count($select); $i++) {
+            for ($i=0; $i<count($select); $i++){
                 $url = $url.$select[$i].", ";
             }
+
         }
 
-        if ($limit>0) {
+        if ($limit>0){
             $url = $url."&\$top=".$limit;
         }
 
-        if ($orderby != null) {
+        if ($orderby != null){
             $url = $url."&\$orderby=".key($orderby)." ".current($orderby);
-        }
-
-        if (is_integer(current($criteria))){
-            $url = $this->model->getUrl()."\?"."\$filter=".key($criteria)." eq ".current($criteria)."";
-            $data =  Connection::Request($url, "GET");
-            return $this->isSingleObject($this->model, $data);
-
         }
 
         $data =  Connection::Request($url, "GET");
 
-        return $this->isArrayCollection($this->model, $data);
+        return $this->isArrayCollection($this->model,$data);
+
     }
 
     /**
     *
     *  @return Object
     */
-    public function find(string $guid)
-    {
+    public function find($guid){
+
         $keyField = $this->getKeyField();
 
         $url  = $this->model->getUrl()."\?"."\$filter=".$keyField." eq guid"."'".$guid."'";
-        $data = Connection::Request($url, "GET");
+        $data = Connection::Request($url,"GET");
 
         return $this->isSingleObject($data);
+
     }
 
     /**
     *
     * @return PrimaryKey field
     */
-    private function getKeyField()
-    {
-        if (method_exists($this->model, "getPrimaryKey")) {
+    private function getKeyField(){
+
+        if ( method_exists($this->model, "getPrimaryKey" ) ){
             $primaryKey = $entity->getPrimaryKey();
-        } else {
+        }else{
             $primaryKey = "ID";
         }
 
@@ -222,33 +219,37 @@ class ExactManager
     /**
     * @return Object
     */
-    private function isSingleObject($data)
-    {
-        $object = $this->model;
-        foreach ($data as $key => $item) {
-            $setter = "set".$key;
-            if (method_exists($object, $setter)) {
-                $object->$setter($item);
+    private function isSingleObject($data){
+
+            $object = $this->model;
+            foreach ($data as $key => $item){
+                    $setter = "set".$key;
+                    if(method_exists($object, $setter)){
+                        $object->$setter($item);
+
+                    }
             }
-        }
-        return $object;
+            return $object;
     }
 
     /**
     * @return Object collection
     */
-    private function isArrayCollection($entity, $data)
-    {
-        foreach ($data as $keyD => $item) {
-            $object = new $entity();
-            foreach ($item as $key => $value) {
-                $setter = "set".$key;
-                if (method_exists($object, $setter)) {
-                    $object->$setter($value);
-                }
+    private function isArrayCollection($entity, $data){
+
+            foreach ($data as $keyD => $item){
+                   $object = new $entity();
+                   foreach($item as $key => $value) {
+                        $setter = "set".$key;
+                        if(method_exists($object, $setter)){
+                            $object->$setter($value);
+                        }
+                    }
+                array_push ($this->list, $object);
             }
-            array_push($this->list, $object);
-        }
         return $this->list;
+
     }
 }
+
+?>
