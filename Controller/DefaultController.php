6f1ad63d64aa4df76656fe2579b41c95bd3a7d89
maxime <maxime@zangra.com>
2020-08-27 14:16:42 +0200

    ZN-V2 Add DefaultController

---
+++ Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ExactOnlineBundle\Controller;
+
+use ExactOnlineBundle\Model\Account;
+use ExactOnlineBundle\Model\Item;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Author: Jefferson Bianchi / Maxime Lambot
+ * Email : Jefferson@aibianchi.com / maxime@zangra.com
+ */
+class DefaultController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $code = $request->query->get('code');
+        $exactManager = $this->get('exact_online.manager');
+        $exactManager->init($code);
+
+        return new JsonResponse('status :: ok');
+    }
+}
