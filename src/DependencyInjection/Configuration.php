457274b0692548b991a8a7bb15f89edfe8cd26d6
nils mechin <nils@zangra.com>
2022-06-07 15:48:10 +0200

    ZN-962 Symfony 5 fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -16,8 +16,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('exact_online');
+        $treeBuilder = new TreeBuilder('exact_online');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
